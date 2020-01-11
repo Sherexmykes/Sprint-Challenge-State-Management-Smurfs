0e080bed24b3384b239377f4b93771bfb699cc30
Sheila R Moore <sherexmykes@github.com>
2020-01-10 22:06:24 -0500

    mvp

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -5,7 +5,6 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import "./index.css";
 import App from "./components/App";
-
 import { smurfReducer } from './reducers/smurfReducer';
 
 const store = createStore(
